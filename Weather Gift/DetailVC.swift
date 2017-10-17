ff4db1428d04c24326af4542cf75c922af76c93a
Erin Boyle <erinboyle@Erins-MacBook.local>
2017-10-17 16:30:07 -0400

    erin boyle - week 8 weather gift

---
+++ Weather Gift/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  Weather Gift
+//
+//  Created by Erin Boyle on 10/16/17.
+//  Copyright © 2017 Erin Boyle. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationLabel.text = locationsArray[currentPage]
+    }
+
+}
